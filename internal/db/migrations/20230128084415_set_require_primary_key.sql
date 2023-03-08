eb6c87e8c410457e288a179866a7f88709c4d6cd
Аскар <askar.nurakhmetov@paybox.money>
2023-03-08 21:04:43 +0600

    updated functional with fixes

--- internal/db/migrations/20230128084415_set_require_primary_key.sql
+++ internal/db/migrations/20230128084415_set_require_primary_key.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-# SET SESSION sql_require_primary_key = 0;
+SET SESSION sql_require_primary_key = 0;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-# SET SESSION sql_require_primary_key = 0;
+SET SESSION sql_require_primary_key = 0;
 -- +goose StatementEnd
